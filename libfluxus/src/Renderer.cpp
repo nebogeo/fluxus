d7777b9aa83721f2d4f785c9d1de143dd06fce0e
Dave Griffiths <dave@pawfal.org>
2007-08-22 20:22:47 +0000

    should really comment it out be default...

--- libfluxus/src/Renderer.cpp
+++ libfluxus/src/Renderer.cpp
@@ -14,7 +14,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-#define TEST_64BIT_TIME
+//#define TEST_64BIT_TIME
 
 #include "Renderer.h"
 #include "State.h"
