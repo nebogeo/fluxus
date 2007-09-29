43555c16f96d7fe047f140a53a605c848af64e01
Dave Griffiths <dave@pawfal.org>
2007-09-29 17:09:44 +0000

    Claude's fixes

--- docs/helpmap2html.scm
+++ docs/helpmap2html.scm
@@ -1,3 +1,4 @@
+(define (clean-id name) (regexp-replace "[!?<>]" name "_"))
 
 (define (write-header locale title htmlfile cssfilename)
   (define (line-loop in out)
@@ -7,7 +8,7 @@
          (fprintf out "~a~n" ret)
          (line-loop in out)))))
 
-  (fprintf htmlfile "<html lang=\"~a\">~n" locale)
+  (fprintf htmlfile "<?xml version=\"1.0\" encoding=\"UTF-8\"?>~n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">~n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"~a\" lang=\"~a\">~n<head>" locale locale)
   (fprintf htmlfile "<title>~a</title>~n" title)
   (fprintf htmlfile "<style type=\"text/css\"><!--~n")
   (let ((cssfile (open-input-file cssfilename)))
@@ -19,19 +20,18 @@
   (fprintf htmlfile "</body>~n</html>~n"))
 
 (define (write-function name args returns desc example htmlfile)
-  (fprintf htmlfile "<a name=\"~a\">" name)
   (if (string=? args "")
-      (fprintf htmlfile "<h2>(~a)</h2></a>" name)
-      (fprintf htmlfile "<h2>(~a ~a)</h2></a>~n" name args))
-  (fprintf htmlfile "<b>Returns</b> ~a<p>~n" returns)
-  (fprintf htmlfile "~a<p>~n" desc)
-  (fprintf htmlfile "<b>Example</b><br>~n")
-  (fprintf htmlfile "<pre>~a</pre><br>~n" example))
+      (fprintf htmlfile "<h2><a name=\"~a\">(~a)</a></h2>" (clean-id name) name)
+      (fprintf htmlfile "<h2><a name=\"~a\">(~a ~a)</a></h2>~n" (clean-id name) name args))
+  (fprintf htmlfile "<p><b>Returns</b> ~a</p>~n" returns)
+  (fprintf htmlfile "<p>~a</p>~n" desc)
+  (fprintf htmlfile "<p><b>Example</b></p>~n")
+  (fprintf htmlfile "<pre>~a</pre>~n" example))
 
 (define (write-section name desc example htmlfile)
   (fprintf htmlfile "<h1>~a</h1>~n" name)
   (fprintf htmlfile "<h3>Description</h3>~n")
-  (fprintf htmlfile "~a~n" desc)
+  (fprintf htmlfile "<p>~a</p>~n" desc)
   (cond 
     ((not (string=? example ""))
      (fprintf htmlfile "<h3>Example</h3>~n")
@@ -44,7 +44,13 @@
   (fprintf htmlfile "<a href=\"~a/index.html\">~a</a> " locale locale))
 
 (define (write-all-function name section htmlfile)
-  (fprintf htmlfile "<a href=\"~a.html#~a\">~a</a> " section name name))
+  (fprintf htmlfile "<a href=\"~a.html#~a\">~a</a> " section (clean-id name) name))
+
+(define (write-functionlist-start htmlfile)
+  (fprintf htmlfile "<p>"))
+
+(define (write-functionlist-end htmlfile)
+  (fprintf htmlfile "</p>"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -70,7 +76,9 @@
        (let ((htmlfile (open-output-file (string-append locale "/" sectionname ".html") 'replace)))
          (write-header locale sectionname htmlfile "fluxusdoc.css")
          (write-section sectionname (car section) (cadr section) htmlfile)
+         (write-functionlist-start indexhtmlfile)
          (parse-function (caddr section) htmlfile indexhtmlfile sectionname)
+         (write-functionlist-end indexhtmlfile)
          (write-footer htmlfile)
          (close-output-port htmlfile)))
      (parse-section (cdr sectionlist) indexhtmlfile locale))))
@@ -94,9 +102,10 @@
   (let ((htmlfile (open-output-file "index.html" 'replace)))
     (write-header "en" "Fluxus Documentation 0.13" htmlfile "fluxusdoc.css")
     (fprintf htmlfile "<h1>Fluxus Documentation 0.13</h1>~n")
-    (fprintf htmlfile "Extracted and html-ised from the runtime documentation system<br>~n")
-    (fprintf htmlfile "Availible languages: ")
+    (fprintf htmlfile "<p>Extracted and html-ised from the runtime documentation system</p>~n")
+    (fprintf htmlfile "<p>Available languages: ")
     (parse-locale (read file) htmlfile)
     (close-input-port file)
+    (fprintf htmlfile "</p>")
     (write-footer htmlfile)
     (close-output-port htmlfile)))
