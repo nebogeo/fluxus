ccab6f83900bfe08ce5f42956409da9a8345b623
Dave Griffiths <dave@pawfal.org>
2008-01-02 20:39:45 +0000

    oops

--- examples/files.scm
+++
@@ -1,9 +0,0 @@
-allprims.scm
-asteroids.scm
-bars.scm
-blobbies.scm
-cube-matrix.scm
-files.scm
-flocking.scm
-GLSL.scm
-GLSL-shaderlib.scm line-prim.scm l-system-obj.scm l-system.scm materials.scm mouse-interactive.scm obj-import.scm osc-test.scm particles.scm physics-joints.scm plenty-o-particles.scm proc-texture.scm qflux-game.scm realtime-toon.scm shadow-phys.scm shadows.scm skinning.scm spotlights.scm sprite-particles.scm texcoords.scm toon.scm tree.scm water-sim.scm wire.scm
