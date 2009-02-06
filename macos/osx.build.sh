2372bd2583dd07c86a3086af4a207a4c2267e219
gabor papp <gabor.papp@gmail.com>
2009-02-06 11:52:52 +0100

    added osx install instructions

--- macos/osx.build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-plt="/Applications/PLT"
-scons -u Prefix=/opt/local/ PLTPrefix="$plt" PLTLib="$plt"/lib PLTInclude="$plt"/include
-sudo scons -u Prefix=/opt/local/ PLTPrefix="$plt" PLTLib="$plt"/lib PLTInclude="$plt"/include install
