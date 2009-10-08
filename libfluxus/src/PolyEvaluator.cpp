8bfb1c235608ff9fe14093d41dde5687e5187d32
Dave Griffiths <dave@pawfal.org>
2009-10-08 16:21:19 +0100

    geo/line-intersect works for tristrips

--- libfluxus/src/PolyEvaluator.cpp
+++ libfluxus/src/PolyEvaluator.cpp
@@ -55,7 +55,25 @@ bool PolyEvaluator::IntersectLine(const dVector &start, const dVector &end, vect
 
 bool PolyEvaluator::IntersectTriStrip(const dVector &start, const dVector &end, vector<Point> &points)
 {
-	return false;
+	dVector bary;
+	bool found=false;
+	unsigned int i=2;
+	while(i<m_Prim->Size())
+	{
+		unsigned int i1=i-2;
+		unsigned int i2=i-1;
+		unsigned int i3=i++;
+		
+		if (IntersectLineTriangle(start,end,m_Prim->GetData<dVector>("p",i1),
+											m_Prim->GetData<dVector>("p",i2),
+											m_Prim->GetData<dVector>("p",i3),bary))
+		{
+			points.push_back(InterpolatePData(bary,i1,i2,i3));
+			found=true;
+		}
+	}
+
+	return found;
 }
 
 bool PolyEvaluator::IntersectQuads(const dVector &start, const dVector &end, vector<Point> &points)
